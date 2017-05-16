132b338db14d4c7a1d5cb9aee593e81c57fbfede
Klorinmannen <jonte.frasse@gmail.com>
2017-05-16 10:17:52 +0200

    okej

--- Assignments/TwitterNethackProject/NewTwitterNethack/NewTwitterNethack/Menu.cpp
+++ Assignments/TwitterNethackProject/NewTwitterNethack/NewTwitterNethack/Menu.cpp
@@ -17,17 +17,21 @@ void Menu::detectKey()
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 	{
 		this->current++;
+		this->menuSprite[2].setPosition(400, 350);
 		if (current > 1)
 		{
 			current = 0;
+			this->menuSprite[2].setPosition(400, 280);
 		}
 	}
 	else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 	{
 		this->current--;
+		this->menuSprite[2].setPosition(400, 280);
 		if (current < 0)
 		{
 			current = 1;
+			this->menuSprite[2].setPosition(400, 350);
 		}
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
@@ -49,9 +53,11 @@ Menu::Menu()
 	this->menuTexture.loadFromFile("Textures\Menu\NewGameButton.png");
 	this->menuSprite[0].setTexture(this->menuTexture);
 	this->menuSprite[0].setPosition(sf::Vector2f(400, 350));
+
 	this->menuTexture.loadFromFile("Textures\Menu\ExitButton.png");
 	this->menuSprite[1].setTexture(this->menuTexture);
 	this->menuSprite[1].setPosition(sf::Vector2f(400, 280));
+
 	this->menuTexture.loadFromFile("Textures\Menu\ButtonHighlight.png");
 	this->menuSprite[2].setTexture(this->menuTexture);
 	this->menuSprite[2].setPosition(sf::Vector2f(400, 350));
